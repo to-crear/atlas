78ad814a041ed86ab8a014648e3a0f81c4e2541d
kenechi Ojukwu <kenechiojukwu@gmail.com>
2023-12-27 14:19:25 +0100

    added initialization functionality

--- atlas/atlas.py
+++ atlas/atlas.py
@@ -1,4 +1,7 @@
+import os
 import click
+import errno
+from .utils.system_utils import get_root_dir
 
 
 @click.group("atlas")
@@ -11,9 +14,22 @@ def atlas() -> None:
 @atlas.command("init")
 def init() -> None:
     """Initialization command"""
-    return None
-
-
+    hidden_dir = ".atlas"
+    setup_folders = ["model_repository","components_ouputs","metadata","inference","manager"]
+    root_dir = get_root_dir(__file__)
+    root_hidden_file = os.path.join(root_dir, hidden_dir)
+    try:
+        os.mkdir(root_hidden_file)
+        for folder in setup_folders:
+            path_ = os.path.join(root_hidden_file, folder)
+            os.mkdir(path_)
+        return 
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            raise Exception("atlas has already being initialized.")
+        else:
+            raise Exception(e.errno)
+            
 @atlas.command("stages")
 def stages() -> None:
     """Print out all the stages in the pipeline"""
