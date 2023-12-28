c63851f8e46dc3b41db6eaa6202b5f84cf48daea
Kene <33866614+kene111@users.noreply.github.com>
2023-12-28 02:14:32 +0100

    cleaned up file

--- atlas/utils/system_utils.py
+++ atlas/utils/system_utils.py
@@ -1,6 +1,5 @@
 import os
-from pathlib import Path
 
 def get_root_dir(file):
     """Get root directory path"""
-    return os.path.abspath(os.curdir)#os.path.dirname(os.path.dirname(os.path.abspath(file)))
+    return os.path.abspath(os.curdir)
