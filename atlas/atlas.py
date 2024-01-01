fb25990c3a58070a6841d5f2cf8110f1619f5c11
Kene <33866614+kene111@users.noreply.github.com>
2024-01-01 21:46:04 +0100

    Initialization (#2)
    * added utils module
    
    * added initialization functionality
    
    * cleaned up file
    
    * removed un-used variables
    
    * used subprocess to make hide .atlas file
    
    * modified code to account for os
    
    * added type hints

--- atlas/atlas.py
+++ atlas/atlas.py
@@ -1,4 +1,9 @@
+import os
 import click
+import errno
+import subprocess
+from sys import platform
+from .utils.system_utils import get_root_dir
 
 
 @click.group("atlas")
@@ -11,9 +16,29 @@ def atlas() -> None:
 @atlas.command("init")
 def init() -> None:
     """Initialization command"""
-    return None
+    hidden_dir = ".atlas"
+    setup_folders = ["model_repository","components_ouputs","metadata","inference","manager"]
+    root_dir = get_root_dir()
+    root_hidden_file = os.path.join(root_dir, hidden_dir)
+    try:
+        os.mkdir(root_hidden_file)
+        for folder in setup_folders:
+            path_ = os.path.join(root_hidden_file, folder)
+            os.mkdir(path_)
+        # make sure hidden folder isn't displayed via UI.
+        if platform == "win32":
+            subprocess.run(["attrib","+H", root_hidden_file], check=True)
+        elif platform == "darwin":
+            subprocess.run(["chflags", "hidden", root_hidden_file], check=True)
 
-
+        click.secho(f"Initialized atlas at {root_hidden_file}", fg="green")
+        return 
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            click.secho(f"Atlas has already been initialized at {root_hidden_file}", fg="red")
+        else:
+            click.secho(f"Error initializing atlas: {e.errno}", fg="red")
+            
 @atlas.command("stages")
 def stages() -> None:
     """Print out all the stages in the pipeline"""
---
+++ atlas/utils/system_utils.py
@@ -0,0 +1,5 @@
+import os
+
+def get_root_dir()->str:
+    """Get root directory path"""
+    return os.path.abspath(os.curdir)
