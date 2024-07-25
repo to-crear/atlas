aba14ebd554f613d09cd7aac02a5a7fae968b7ab
kenechi Ojukwu <kenechiojukwu@gmail.com>
2024-07-25 13:49:40 +0100

    updated root folder path retrival logic

--- atlas/atlas_manager.py
+++ atlas/atlas_manager.py
@@ -6,6 +6,7 @@ import click
 class AtlasManagerError(Exception):
     """Error when calling atlas manager class functions"""
 
+
 class AtlasManager:
 
     def __init__(self, system_root_folder=None):
@@ -17,26 +18,29 @@ class AtlasManager:
             self.system_root_folder = system_root_folder
             tmp_info = {"project_root_path": self.system_root_folder}
             self.save_info_to_project_json(tmp_info)
-            
+
         json_folder_path_ = os.path.join(self.project_folder, self.project_json)
-        self.project_json_path = os.path.join(self.system_root_folder, json_folder_path_)
+        self.project_json_path = os.path.join(
+            self.system_root_folder, json_folder_path_
+        )
 
     def get_system_root_folder_path(self):
-        """Get system root folder 
+        """Get system root folder
 
         Parameters
         ----------
-    
+
         Returns
         -------
         : bool
         """
         from .utils.system_utils import get_root_dir
         from config.atlas_config import ATLAS_HIDDEN_DIRECTORY
+
         root_dir = get_root_dir()
         root_hidden_file = os.path.join(root_dir, ATLAS_HIDDEN_DIRECTORY)
         return root_hidden_file
-    
+
     def save_info_to_project_json(self, info: dict) -> True:
         """Saves information to project json file.
 
@@ -54,8 +58,8 @@ class AtlasManager:
             return
 
         try:
-            with open(self.project_json_path,"w") as json_file:
+            with open(self.project_json_path, "w") as json_file:
                 json.dump(info, json_file)
             return True
         except:
-            raise  AtlasManagerError(f"Error saving information to {self.project_json}.")
+            raise AtlasManagerError(f"Error saving information to {self.project_json}.")
