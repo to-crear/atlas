b244a654a593a546dcdaa9cba273d99b831372d7
kenechi Ojukwu <kenechiojukwu@gmail.com>
2024-08-07 23:26:56 +0100

    updated recursive logic

--- atlas/atlas_manager.py
+++ atlas/atlas_manager.py
@@ -1,8 +1,10 @@
 import json
 import os
-from typing import Optional, Union
+from typing import Optional, Union, Any
 
 from config.atlas_config import ATLAS_HIDDEN_DIRECTORY
+
+from .utils.system_utils import get_root_dir
 
 
 class AtlasManagerError(Exception):
@@ -29,28 +31,38 @@ class AtlasManager:
             tmp_info = {"project_root_path": self.system_root_folder}
             self.save_info_to_project_json(tmp_info)
 
-    def __search_for_dot_atlas_folder_path(self) -> Optional[Union[str, None]]:
+    def __search_for_dot_atlas_folder_path(self, root_path: str = None) -> Any:
         """Recursively Search for .atlas directory.
 
         Parameters
         ----------
+        root_path: str
+            Root directory path.
 
         Returns
         -------
-        : bool
+        : str
         """
         dot_atlas_path = None
 
-        try:
-            for root, dirs, _ in os.walk("."):
-                for dir_ in dirs:
-                    if dir_ == ATLAS_HIDDEN_DIRECTORY:
-                        root_dir = os.path.join(root, dir_)
-                        dot_atlas_path = os.path.abspath(root_dir)
-                        return dot_atlas_path
-        except Exception as e:
-            raise AtlasManagerError(f"'.atlas' Search Error: {e}")
-        return None
+        if root_path is None:
+
+            cur_dir = get_root_dir()
+        else:
+            cur_dir = root_path
+
+        cur_dir_content_list = os.listdir(cur_dir)
+
+        for dir_ in cur_dir_content_list:
+            if dir_ == ATLAS_HIDDEN_DIRECTORY:
+                dot_atlas_path = os.path.join(cur_dir, dir_)
+                return dot_atlas_path
+
+            if dir_ == cur_dir_content_list[-1]:
+                path = os.path.split(cur_dir)
+                if path[0] == "C:\\" or path[0] == "/c/":
+                    raise AtlasManagerError(".atlas folder could not be found!")
+                return self.__search_for_dot_atlas_folder_path(path[0])
 
     def get_project_dot_atlas_folder_path(self):
         """Get system root folder
