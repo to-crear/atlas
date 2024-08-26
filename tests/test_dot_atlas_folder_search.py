bc4d24e03b71cc502782c830f036800960de06ea
kenechi Ojukwu <kenechiojukwu@gmail.com>
2024-08-26 21:37:27 +0100

    added test logic

--- tests/test_dot_atlas_folder_search.py
+++ tests/test_dot_atlas_folder_search.py
@@ -1,11 +1,18 @@
+import os
 import pytest
-
 from atlas.atlas_manager import AtlasManager, AtlasManagerError
 
 
-def test_dot_atlas_folder_search():
-    pass
-    # try:
-    #     AtlasManager()
-    # except Exception as e:
-    #     raise AtlasManagerError(f"Test:Error: {e}")
+def test_dot_atlas_folder_search(tmp_path):
+    tmp_root_dir = tmp_path 
+    dot_atlas_dir = tmp_root_dir / ".atlas" 
+    dot_atlas_dir.mkdir()
+    dot_atlas_manager =  dot_atlas_dir / "manager"
+    dot_atlas_manager.mkdir()
+    assert type(AtlasManager(str(dot_atlas_dir)).project_json_path)  == str
+   
+
+def test_dot_atlas_folder_search_non_existent(tmp_path):
+    with pytest.raises(AtlasManagerError, match='Error saving information to atlas_project.json.'):
+        AtlasManager(str(tmp_path))
+   
